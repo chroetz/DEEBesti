10951fc4ca50d2fc4e516807c4f6c7be3a830ec3
Christof Schoetz <cschoetz@pik-potsdam.de>
2023-11-17 11:11:55 +0100

    fix

--- R/copyTruth.R
+++ R/copyTruth.R
@@ -10,19 +10,15 @@ copyTruth <- function(
   for (model in models) {
     paths <- DEEBpath::getPaths(dbPath, model, example=example)
     cat(model, "\n")
-    truthMeta <- DEEBpath::getMetaGeneric(
-      paths$truth,
-      c("task", "truth"),
+    meta <- DEEBpath::getMetaGeneric(
+      c(paths$obs, paths$truth),
+      tagFileFilter = list(c("truth", "obs"), c("task", "truth")),
       nrFilters = list(obsNr = obsNrFilter, truthNr = truthNrFilter))
-    obsMeta <- DEEBpath::getMetaGeneric(
-      paths$obs,
-      c("truth", "obs"),
-      nrFilters = list(obsNr = obsNrFilter, truthNr = truthNrFilter))
-    meta <- dplyr::left_join(obsMeta, truthMeta, by = "truthNr", multiple = "all")
     outPath <- file.path(paths$esti, "Truth")
     dir.create(outPath, showWarnings=FALSE, recursive=TRUE)
     for (i in seq_len(nrow(meta))) {
       info <- as.list(meta[i,])
+      cat("\ttruth:", info$truthNr, ", obs:", info$obsNr, ", task:", info$taskNr, "\n")
       file.copy(info$truthPath, file.path(outPath, DEEBpath::estiFile(info)))
     }
   }
